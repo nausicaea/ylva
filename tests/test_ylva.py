43cac6e9ed7017d0c85b608d3de1f7527d136203
Eleanor Young <developer@nausicaea.net>
2022-08-31 19:35:25 +0200

    Implemented the budget listing endpoint

--- tests/test_ylva.py
+++ tests/test_ylva.py
@@ -1,38 +1,83 @@
-from typing import Generic, List, Type, TypeVar
+from dataclasses import dataclass, field
+from datetime import date, datetime
+from typing import Generic, List, Optional, Type, TypeVar
 
 import mt940
 import pytest
 import reidun.client
+from mashumaro.helper import field_options
+from mashumaro.mixins.json import DataClassJSONMixin
 from reidun.auth_method import BearerAuth
 from reidun.endpoint import ApiEndpoint, ParamsBuilder
-from reidun.serialization import PermissiveSchema, SerializableData
-from swagger_client.api.budgets_api import BudgetsApi
-from swagger_client.api_client import ApiClient
 
 from ylva import __version__
 
 _TOKEN: str = "***REMOVED***"
 
 
-T = TypeVar("T", bound=SerializableData)
+T = TypeVar("T")
 
 
 @dataclass
-class BudgetsEntry(SerializableData):
-    pass
+class DateFormat(DataClassJSONMixin):
+    format_: str = field(metadata=field_options(alias="format"))
 
 
 @dataclass
-class Budgets(SerializableData):
+class CurrencyFormat(DataClassJSONMixin):
+    iso_code: str
+    example_format: str
+    decimal_digits: int
+    decimal_separator: str
+    symbol_first: bool
+    group_separator: str
+    currency_symbol: str
+    display_symbol: bool
+
+
+@dataclass
+class BudgetAccountsEntry(DataClassJSONMixin):
+    id_: str = field(metadata=field_options(alias="id"))
+    name: str
+    type_: str = field(metadata=field_options(alias="type"))
+    on_budget: bool
+    closed: bool
+    note: str
+    balance: float
+    cleared_balance: float
+    uncleared_balance: float
+    transfer_payee_id: str
+    direct_import_linked: bool
+    direct_import_in_error: bool
+    deleted: bool
+
+
+@dataclass
+class BudgetsEntry(DataClassJSONMixin):
+    id_: str = field(metadata=field_options(alias="id"))
+    name: str
+    last_modified_on: datetime
+    first_month: date
+    last_month: date
+    date_format: DateFormat
+    currency_format: CurrencyFormat
+    accounts: Optional[List[BudgetAccountsEntry]] = field(default=None)
+
+
+@dataclass
+class BudgetsList(DataClassJSONMixin):
     budgets: List[BudgetsEntry]
+    default_budget: Optional[BudgetsEntry]
 
 
-class ResponseWrapper(SerializableData, Generic[T]):
-    class ResponseWrapperSchema(MSchema):
-        data = fields.Nested(T.Schema)
-
+@dataclass
+class ResponseWrapper(Generic[T]):
     data: T
-    Schema: Type[MSchema] = ResponseWrapperSchema
+
+
+@dataclass
+class Budgets(ResponseWrapper[BudgetsList], DataClassJSONMixin):
+    pass
 
 
 class ListBudgets(ApiEndpoint):
@@ -42,10 +87,10 @@ class ListBudgets(ApiEndpoint):
     def path(self) -> str:
         return "/v1/budgets"
 
-    def response_data_type(self) -> Type[ResponseWrapper[Budgets]]:
-        return ResponseWrapper[Budgets]
+    def response_data_type(self) -> Type[Budgets]:
+        return Budgets
 
-    def request_data_type(self) -> Type[SerializableData]:
+    def request_data_type(self) -> Type[DataClassJSONMixin]:
         raise NotImplementedError()
 
 
@@ -61,19 +106,10 @@ def test_load_mt940() -> None:
         print(data)
 
 
-@pytest.mark.xfail
-@pytest.mark.asyncio
-async def test_get_budgets() -> None:
-    client = ApiClient(header_name="Authorization", header_value=f"Bearer {_TOKEN}")
-    budgets_api = BudgetsApi(api_client=client)
-    data = budgets_api.get_budgets()
-    print(data)
-
-
 @pytest.mark.asyncio
 async def test_create_client() -> None:
     async with reidun.client.ApiClient(
         "https://api.youneedabudget.com", auth=BearerAuth(_TOKEN)
     ) as client:
-        data = await client.get(ListBudgets())
+        data, _ = await client.get(ListBudgets())
         print(data)
